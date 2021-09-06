9a281af20af3e4d928108adec3f50f3758d61dc6
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-06 10:51:22 +0700

    Set up the project

---
+++ swift-travel/swift_travelApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_travelApp.swift
+//  swift-travel
+//
+//  Created by Danh Tu on 05/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_travelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(location: Location.example)
+        }
+    }
+}
